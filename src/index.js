d101ea033cfd082412875bb6e0f2a5e80d5a78e7
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-08-23 23:15:54 +0300

    feat: fixed bug that caused error by loading app to a hosting

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/pets-paw">
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
